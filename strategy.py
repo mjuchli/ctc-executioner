4f1f61b22162de9aca1b5cc61797e9410e61c852
Marc Juchli <mail@marcjuch.li>
2018-01-31 01:58:10 +0100

    Implemented execution behaviour

--- strategy.py
+++ strategy.py
@@ -11,6 +11,7 @@ import matplotlib.animation as animation
 import seaborn as sns
 sns.set(color_codes=True)
 
+
 def getAvgPriceDiffForInventory(M, inventory_observe):
     ms = [x for x in M if x[0][0] != 0 and x[0][1] == inventory_observe] # filter market orders (t==0)
     price_diffs = [x[4] for x in ms]
@@ -41,21 +42,21 @@ def train(episodes=100):
     return actionSpace.ai.q
 
 
-def test(episodes=100, average=True):
+def test(episodes=100, average=True, fixed_a=None):
     if not orderbook_test.getStates():
         orderbook_test.loadFromFile(testBook)
 
     q = np.load('q.npy').item()
     # M <- [t, i, Price, A, Paid, Diff]
-    M = actionSpace_test.backtest(q, episodes, average=average)
+    M = actionSpace_test.backtest(q, episodes, average=average, fixed_a=fixed_a)
     return M
 
 
-def animate(f, interval=5000, axis=[0, 100, -50, 50]):
+def animate(f, interval=5000, axis=[0, 100, -50, 50], frames=None):
     fig = plt.figure()
     ax1 = fig.add_subplot(1,1,1)
     ax1.axis(axis)
-    ax1.autoscale(False)
+    ax1.autoscale(True)
     xs = []
     ys = []
 
@@ -72,31 +73,35 @@ def animate(f, interval=5000, axis=[0, 100, -50, 50]):
     ani = animation.FuncAnimation(
         fig,
         lambda i: do_animate(i, f, ax1, xs, ys),
-        interval=interval
+        interval=interval,
+        frames=frames
     )
+    # from IPython.display import HTML
+    # HTML(ani.to_jshtml())
     plt.show()
 
 
-def run_profit():
-    q = train(1)
-    M = test(5, average=False)
+def run_profit(epochs_train=0, epochs_test=5, fixed_a=None):
+    if epochs_train > 0:
+        q = train(epochs_train)
+    M = test(epochs_test, average=False, fixed_a=fixed_a)
     M = np.array(M)
     print(M)
     return np.mean(M[0:, 4])
 
 
-def calculate_profits(epochs):
+def calculate_profits(epochs, fixed_a=None):
     profits = []
     for i in range(epochs):
-        M = test(1, average=False)
+        M = test(1, average=False, fixed_a=fixed_a)
         M = np.array(M)
-        print(M)
+        #print(M)
         profits.append(np.sum(M[0:, 4]))
     return profits
 
 
-def hist_profit(episodes):
-    x = calculate_profits(episodes)
+def hist_profit(episodes, fixed_a=None):
+    x = calculate_profits(episodes, fixed_a=fixed_a)
     sns.distplot(x)
     plt.show()
 
@@ -108,41 +113,62 @@ def run_q_reward():
     return reward
 
 
-pp = pprint.PrettyPrinter(indent=4)
-# logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.DEBUG)
 trainBook = 'query_result_train_15m.tsv'
-testBook = 'query_result_test_15m.tsv'
+testBook = 'query_result_train_15m.tsv'
 
 side = OrderSide.BUY
 levels = [5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -10, -12, -15]
 ai = QLearn(actions=levels, epsilon=0.4, alpha=0.3, gamma=0.8)
 
-orderbook = Orderbook(extraFeatures=False)
-orderbook_test = Orderbook(extraFeatures=False)
+orderbook = Orderbook(extraFeatures=True)
+#orderbook.loadFromBitfinexFile('orderbook_bitfinex_btcusd_view.tsv')
+orderbook_test = Orderbook(extraFeatures=True)
+#orderbook_test.loadFromBitfinexFile('orderbook_bitfinex_btcusd_view.tsv')
 
 T = [10, 20, 40, 60, 80, 100] #, 120, 240]
-T_test = [0, 20, 40, 60, 80, 100] #, 120, 240]
+T_test = [0, 10, 20, 40, 60, 80, 100]# 120, 240]
 
 I = [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 actionSpace = ActionSpace(orderbook, side, T, I, ai, levels)
 actionSpace_test = ActionSpace(orderbook_test, side, T_test, I, ai, levels)
 
-train(50)
-hist_profit(100)
+def priceReturnCurve(levels=range(-50, 51), crossval=10, force_execution=True):
+    y = []
+    y2 = []
+    for level in levels:
+        profit = []
+        profit2 = []
+        t = T[-1]
+        i = I[-1]
+        a = level
+        for _ in range(crossval):
+            action = actionSpace.createAction(a, t, i, force_execution=force_execution)
+            refBefore = action.getReferencePrice()
+            action.run(actionSpace.orderbook)
+            refAfter = action.getOrderbookState().getTradePrice()
+            paid = action.getAvgPrice()
+            # print("ref before: " + str(refBefore))
+            # print("ref after: " + str(refAfter))
+            # print("paid: " + str(paid))
+            if paid == 0.0:
+                continue
+            elif action.getOrder().getSide() == OrderSide.BUY:
+                profit.append(refBefore - paid)
+                profit2.append(refAfter - paid)
+            else:
+                profit.append(paid - refBefore)
+                profit2.append(paid - refAfter)
 
-#animate(run_profit, interval=100)
+        y.append(np.mean(profit))
+        y2.append(np.mean(profit2))
+    x = levels
+    plt.plot(x, y)
+    plt.plot(x, y2)
+    plt.show()
+
+
+#train(1)
+#priceReturnCurve(crossval=1)
+animate(run_profit, interval=100)
 #animate(run_q_reward, interval=1000)
-
-
-I = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
-actionSpace = ActionSpace(orderbook, side, T, I, ai, levels)
-actionSpace_test = ActionSpace(orderbook_test, side, T_test, I, ai, levels)
-train(50)
-hist_profit(100)
-
-I = [0.01, 0.02, 0.03, 0.04, 0.05]
-actionSpace = ActionSpace(orderbook, side, T, I, ai, levels)
-actionSpace_test = ActionSpace(orderbook_test, side, T_test, I, ai, levels)
-train(50)
-hist_profit(100)
-hist_profit(100)
