1e0032fe6c1d8346f224ec281f0a4bb1049cd2c4
Marc Juchli <mail@marcjuch.li>
2018-04-28 04:03:34 +0200

    Make marketmaker a child class from executioner

--- gym_ctc_marketmaker/envs/marketmaker_env.py
+++ gym_ctc_marketmaker/envs/marketmaker_env.py
@@ -1,20 +1,15 @@
 import logging
-import copy
-import random
-import numpy as np
-import gym
-from gym import error, spaces, utils
-from gym.utils import seeding
+import itertools
+from gym import spaces
 from ctc_executioner.action import Action
 from ctc_executioner.action_state import ActionState
 from ctc_executioner.order import Order
 from ctc_executioner.order_type import OrderType
 from ctc_executioner.order_side import OrderSide
-import itertools
-
+import gym_ctc_executioner.envs.execution_env as execution_env
 #logging.basicConfig(level=logging.INFO)
 
-class MarketMakerEnv(gym.Env):
+class MarketMakerEnv(execution_env.ExecutionEnv):
 
     def __init__(self):
         self.orderbookIndexBuy = None
@@ -25,22 +20,8 @@ class MarketMakerEnv(gym.Env):
         self.executionSell = None
         self._configure()
 
-    def _generate_Sequence(self, min, max, step):
-        """ Generate sequence (that unlike xrange supports float)
-
-        max: defines the sequence maximum
-        step: defines the interval
-        """
-        i = min
-        I = []
-        while i <= max:
-            I.append(i)
-            i = i + step
-        return I
-
     def _configure(self,
                    orderbook=None,
-                   side=OrderSide.SELL,
                    levels=(-50, 50, 1),
                    T=(0, 100, 10),
                    I=(0, 1, 0.1),
@@ -48,7 +29,6 @@ class MarketMakerEnv(gym.Env):
                    bookSize=10
                    ):
         self.orderbook = orderbook
-        self.side = OrderSide.SELL
         self.levels = self._generate_Sequence(min=levels[0], max=levels[1], step=levels[2])
         self.levels = list(itertools.product(self.levels, self.levels))
         self.T = self._generate_Sequence(min=T[0], max=T[1], step=T[2])
@@ -58,68 +38,8 @@ class MarketMakerEnv(gym.Env):
         self.action_space = spaces.Discrete(len(self.levels))
         self.observation_space = spaces.Box(low=0.0, high=10.0, shape=(2*self.lookback, self.bookSize, 2))
 
-    def setOrderbook(self, orderbook):
-        #self.orderbookOriginal = orderbook
-        #self.orderbook = copy.deepcopy(self.orderbookOriginal)
-        self.orderbook = orderbook
-
     def setSide(self, side):
-        self.side = side
-
-    def setLevels(self, min, max, step):
-        self.levels = self._generate_Sequence(min=min, max=max, step=step)
-        self.action_space = spaces.Discrete(len(self.levels))
-
-    def setT(self, min, max, step):
-        self.T = self._generate_Sequence(min=min, max=max, step=step)
-
-    def setI(self, min, max, step):
-        self.I = self._generate_Sequence(min=min, max=max, step=step)
-
-    def setLookback(self, lookback):
-        self.lookback = lookback
-        if self.bookSize is not None:
-            self.observation_space = spaces.Box(low=0.0, high=10.0, shape=(2*self.lookback, self.bookSize, 2))
-
-    def setBookSize(self, bookSize):
-        self.bookSize = bookSize
-        if self.lookback is not None:
-            self.observation_space = spaces.Box(low=0.0, high=10.0, shape=(2*self.lookback, self.bookSize, 2))
-
-    def _determine_next_inventory(self, execution):
-        qty_remaining = execution.getQtyNotExecuted()
-        # TODO: Working with floats requires such an ugly threshold
-        if qty_remaining > 0.0000001:
-            # Approximate next closest inventory given remaining and I
-            i_next = min([0.0] + self.I, key=lambda x: abs(x - qty_remaining))
-            logging.info('Qty remain: ' + str(qty_remaining)
-                         + ' -> inventory: ' + str(qty_remaining)
-                         + ' -> next i: ' + str(i_next))
-        else:
-            i_next = 0.0
-
-        logging.info('Next inventory for execution: ' + str(i_next))
-        return i_next
-
-    def _determine_next_time(self, t):
-        if t > 0:
-            t_next = self.T[self.T.index(t) - 1]
-        else:
-            t_next = t
-
-        logging.info('Next timestep for execution: ' + str(t_next))
-        return t_next
-
-    def _determine_runtime(self, t):
-        if t != 0:
-            T_index = self.T.index(t)
-            runtime = self.T[T_index] - self.T[T_index - 1]
-        else:
-            runtime = t
-        return runtime
-
-    def _get_random_orderbook_state(self):
-        return self.orderbook.getRandomState(runtime=max(self.T), min_head=self.lookback)
+        pass
 
     def _create_execution(self, a, actionState, orderbookIndex, side):
         runtime = self._determine_runtime(actionState.getT())
@@ -169,17 +89,6 @@ class MarketMakerEnv(gym.Env):
         execution.setOrderbookState(orderbookState)
         execution.setOrderbookIndex(orderbookIndex)
         return execution
-
-    def _makeFeature(self, orderbookIndex):
-        return self.orderbook.getBidAskFeatures(
-            state_index=orderbookIndex,
-            lookback=self.lookback,
-            qty=self.I[-1],#i_next+0.0001,
-            normalize=True,
-            price=True,
-            size=True,
-            levels = self.bookSize
-        )
 
     def step(self, action):
         # print('action')
@@ -275,8 +184,3 @@ class MarketMakerEnv(gym.Env):
 
     def seed(self, seed):
         pass
-
-
-# import gym_ctc_executioner
-# env = gym.make("ctc-executioner-v0")
-# env.reset()
